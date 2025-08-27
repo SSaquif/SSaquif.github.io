d102f76b2cdfa2e5ec80e6138163dbfea61fe036
Sadnan Saquif <ssaquif@gmail.com>
2025-08-26 20:13:57 -0400

    Divided AboutMe Section to separate the contacts

--- src/components/Portfolio/AboutMeSection.tsx
+++ src/components/Portfolio/AboutMeSection.tsx
@@ -1,6 +1,4 @@
-import Emoji from "a11y-react-emoji";
-import { Star, Sparkles } from "lucide-react";
-import { Phone, Mail } from "lucide-react";
+import { Star } from "lucide-react";
 import type { AboutMe } from "../../data/AboutMe";
 import { styled } from "../../stitches.config";
 
@@ -11,157 +9,30 @@ interface AboutMeCardProps {
 export function AboutMeSection({ data }: AboutMeCardProps) {
   return (
     <Container>
-      <GeneralInfoContainer>
-        <h1>{data.name}</h1>
-        <h2>{data.title}</h2>
-        <SocialsContainer>
-          {data.socials &&
-            Object.entries(data.socials).map(([key, social]) => (
-              <li key={key}>
-                <a href={social.url} target="_blank" rel="noopener noreferrer">
-                  {social.icon}
-                </a>
-              </li>
-            ))}
-        </SocialsContainer>
-        <ContactsContainer>
-          <ContactHeaderColumn>
-            {/* <p>Emails</p> */}
-            <Mail />
-          </ContactHeaderColumn>
-          <ContactItemColumn>
-            {data.contacts.map((contact) =>
-              contact.emails.map((email) => <li key={email}>{email}</li>)
-            )}
-          </ContactItemColumn>
-          <ContactHeaderColumn>
-            {/* <p>Phone</p> */}
-            <Phone />
-          </ContactHeaderColumn>
-          <ContactItemColumn>
-            {data.contacts.map((contact) =>
-              contact.phone.map((phone) => (
-                <li
-                  key={phone.number}
-                  style={{
-                    verticalAlign: "middle",
-                  }}
-                >
-                  <Emoji
-                    symbol="🇨🇦"
-                    label="canada-flag"
-                    style={{
-                      verticalAlign: "-5%",
-                    }}
-                  />{" "}
-                  {phone.number}
-                </li>
-              ))
-            )}
-          </ContactItemColumn>
-        </ContactsContainer>
-      </GeneralInfoContainer>
-      <DescriptionContainer>
-        <p>{data.description}</p>
-        {/* </Card> */}
-        {/* <Card> */}
-        <ul>
-          {data.qualificationSummary.map((item, index) => (
-            //@TODO: Figure out why only inline style works here with <li/> and contents
-            <li
-              key={index}
-              style={{
-                lineHeight: "1.25",
-                display: "grid",
-                gridTemplateColumns: "auto 1fr",
-                gap: "0.75rem",
-                alignItems: "center",
-              }}
-            >
-              <Star size={20} />
-              {item}
-            </li>
-          ))}
-        </ul>
-      </DescriptionContainer>
+      <p>{data.description}</p>
+      <ul>
+        {data.qualificationSummary.map((item, index) => (
+          //@TODO: Figure out why only inline style works here with <li/> and contents
+          <li
+            key={index}
+            style={{
+              lineHeight: "1.25",
+              display: "grid",
+              gridTemplateColumns: "auto 1fr",
+              gap: "0.75rem",
+              alignItems: "center",
+            }}
+          >
+            <Star size={20} />
+            {item}
+          </li>
+        ))}
+      </ul>
     </Container>
   );
 }
 
 const Container = styled("div", {
-  // border: "1px solid red",
-  color: "$primaryText",
-});
-
-const GeneralInfoContainer = styled("section", {
-  // border: "1px solid $border",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  textAlign: "center",
-  gap: "0.5rem",
-  // border: "1px solid hotpink",
-});
-
-// 2 rows and 2 columns grid
-const ContactsContainer = styled("div", {
-  display: "grid",
-  placeSelf: "center",
-  // border: "1px solid red",
-  gridTemplateColumns: "15% 85%",
-  gridTemplateRows: "1fr 1fr",
-  minWidth: "270px",
-  "@mobileAndDown": {
-    width: "95%",
-  },
-});
-
-const ContactHeaderColumn = styled("div", {
-  display: "flex",
-  flexDirection: "row",
-  gap: "0.25rem",
-  // borderRight: "2px solid $border",
-  alignItems: "center",
-  justifyContent: "center",
-  // border: "1px solid $border",
-
-  "@mobileAndDown": {
-    flexDirection: "column",
-    // alignItems: "center",
-    // justifyContent: "center",
-    gap: "0.1rem",
-  },
-});
-
-const ContactItemColumn = styled("ul", {
-  listStyle: "none",
-  padding: 0,
-  margin: 0,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  // border: "1px solid $border",
-
-  "@mobileAndDown": {
-    display: "flex",
-    flexDirection: "column",
-    // alignItems: "flex-start",
-    padding: "0 0.5rem",
-    justifyContent: "center",
-    // gap: "2px",
-  },
-});
-
-const SocialsContainer = styled("ul", {
-  listStyle: "none",
-  display: "flex",
-  flexDirection: "row",
-  gap: "1rem",
-  padding: 0,
-});
-
-const DescriptionContainer = styled("div", {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
---
+++ src/components/Portfolio/IntroSection.tsx
@@ -0,0 +1,131 @@
+import Emoji from "a11y-react-emoji";
+import { Phone, Mail } from "lucide-react";
+import type { AboutMe } from "../../data/AboutMe";
+import { styled } from "../../stitches.config";
+
+interface IntroProps {
+  data: AboutMe;
+}
+
+export function IntroSection({ data }: IntroProps) {
+  return (
+    <Container>
+      <h1>{data.name}</h1>
+      <h2>{data.title}</h2>
+      <SocialsContainer>
+        {data.socials &&
+          Object.entries(data.socials).map(([key, social]) => (
+            <li key={key}>
+              <a href={social.url} target="_blank" rel="noopener noreferrer">
+                {social.icon}
+              </a>
+            </li>
+          ))}
+      </SocialsContainer>
+      <ContactsContainer>
+        <ContactHeaderColumn>
+          {/* <p>Emails</p> */}
+          <Mail />
+        </ContactHeaderColumn>
+        <ContactItemColumn>
+          {data.contacts.map((contact) =>
+            contact.emails.map((email) => <li key={email}>{email}</li>)
+          )}
+        </ContactItemColumn>
+        <ContactHeaderColumn>
+          {/* <p>Phone</p> */}
+          <Phone />
+        </ContactHeaderColumn>
+        <ContactItemColumn>
+          {data.contacts.map((contact) =>
+            contact.phone.map((phone) => (
+              <li
+                key={phone.number}
+                style={{
+                  verticalAlign: "middle",
+                }}
+              >
+                <Emoji
+                  symbol="🇨🇦"
+                  label="canada-flag"
+                  style={{
+                    verticalAlign: "-5%",
+                  }}
+                />{" "}
+                {phone.number}
+              </li>
+            ))
+          )}
+        </ContactItemColumn>
+      </ContactsContainer>
+    </Container>
+  );
+}
+
+const Container = styled("section", {
+  // border: "1px solid $border",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  textAlign: "center",
+  gap: "0.5rem",
+  // border: "1px solid hotpink",
+});
+
+// 2 rows and 2 columns grid
+const ContactsContainer = styled("div", {
+  display: "grid",
+  placeSelf: "center",
+  // border: "1px solid red",
+  gridTemplateColumns: "15% 85%",
+  gridTemplateRows: "1fr 1fr",
+  minWidth: "270px",
+  "@mobileAndDown": {
+    width: "95%",
+  },
+});
+
+const ContactHeaderColumn = styled("div", {
+  display: "flex",
+  flexDirection: "row",
+  gap: "0.25rem",
+  // borderRight: "2px solid $border",
+  alignItems: "center",
+  justifyContent: "center",
+  // border: "1px solid $border",
+
+  "@mobileAndDown": {
+    flexDirection: "column",
+    // alignItems: "center",
+    // justifyContent: "center",
+    gap: "0.1rem",
+  },
+});
+
+const ContactItemColumn = styled("ul", {
+  listStyle: "none",
+  padding: 0,
+  margin: 0,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  // border: "1px solid $border",
+
+  "@mobileAndDown": {
+    display: "flex",
+    flexDirection: "column",
+    // alignItems: "flex-start",
+    padding: "0 0.5rem",
+    justifyContent: "center",
+    // gap: "2px",
+  },
+});
+
+const SocialsContainer = styled("ul", {
+  listStyle: "none",
+  display: "flex",
+  flexDirection: "row",
+  gap: "1rem",
+  padding: 0,
+});
